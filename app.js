3488331587b9ef857424c1193e67e430f7e50e94
Azadcelik <azadcelikk12@gmail.com>
2024-12-28 10:58:22 -0500

    query in express

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
-// const readingFile = require('fs');
+// // const readingFile = require('fs');
 
-// for (let i = 0; i < 1000; i++) { 
-//     readingFile.writeFileSync('./subfolder/big.txt',`this is my 1000k file ${i}\n `, {flag: 'a'} )
-// }
+// // for (let i = 0; i < 1000; i++) { 
+// //     readingFile.writeFileSync('./subfolder/big.txt',`this is my 1000k file ${i}\n `, {flag: 'a'} )
+// // }
 
 
-// const { createReadStream } =  require('fs');
-// const stream = createReadStream('./subfolder/big.txt',{highWaterMark: 9000,encoding: 'utf-8'});
-// stream.on('data',(chunk) => console.log(chunk));
+// // const { createReadStream } =  require('fs');
+// // const stream = createReadStream('./subfolder/big.txt',{highWaterMark: 9000,encoding: 'utf-8'});
+// // stream.on('data',(chunk) => console.log(chunk));
 
-// stream.on('error',(err) => console.log(err))
+// // stream.on('error',(err) => console.log(err))
+
+// const http = require('http');
+// const fs = require('fs');
+
+
+// http.createServer(function (req,res) { 
+//     //without chunk if you check network you will see the number of size 
+//     // const text = fs.readFileSync('./subfolder/big.txt','utf-8');
+//     // res.end(text);
+
+//     // with stream when you check size you would only see chunked 
+//     const fileStream = fs.createReadStream('./subfolder/big.txt','utf-8')
+
+//     fileStream.on('open', () => { 
+//         fileStream.pipe(res);
+//     })
+
+//     fileStream.on('error',(err) => {
+//         res.end(err)
+//     })
+// }).listen(5005)
+
+// console.log(require.cache)
+
 
 const http = require('http');
-const fs = require('fs');
 
 
-http.createServer(function (req,res) { 
-    //without chunk if you check network you will see the number of size 
-    // const text = fs.readFileSync('./subfolder/big.txt','utf-8');
-    // res.end(text);
 
-    // with stream when you check size you would only see chunked 
-    const fileStream = fs.createReadStream('./subfolder/big.txt','utf-8')
+const server = http.createServer((req,res) => { 
 
-    fileStream.on('open', () => { 
-        fileStream.pipe(res);
-    })
 
-    fileStream.on('error',(err) => {
-        res.end(err)
-    })
-}).listen(5005)
+})
 
-console.log(require.cache)
+
