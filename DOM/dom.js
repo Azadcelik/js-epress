5a54d862dc8a87c9fd0fe247caa76e3ccee677d9
Azadcelik <azadcelikk12@gmail.com>
2025-01-09 19:42:08 -0500

    dom introduction

---
+++ DOM/dom.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Code that uses the document object
+    // console.dir(document) //it is gonna show all different properties and methods that attach to the document objects
+    // console.log(typeof document)
+    
+    // console.log(document.domain);
+    // console.log(document.URL);
+    // console.log(document.doctype)
+    // console.log(document.head)
+    // console.log(document.body)
+    // console.log(document.styleSheets)
+
+
+    // console.log(document.title)
+    // console.log(document.all[7][1])
+    
+    // console.log(document.forms)
+    // console.log(document.links)
+
+
+
+
+});
