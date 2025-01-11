b4b478301fae5edefa8ebfdff1439f84a37f8bea
Azadcelik <azadcelikk12@gmail.com>
2025-01-11 10:02:45 -0500

    querySelector

--- DOM/dom.js
+++ DOM/dom.js
@@ -25,5 +25,51 @@ document.addEventListener('DOMContentLoaded', () => {
     // console.log(firstText.innerHTML);
     // firstText.style.color = "red"
 
+    //GETELEMENTBYCLASSNAME => selects all tag with same class name 
+    // let list = document.getElementsByClassName('list-items');
+    // list[1].textContent = "changed";
+    // list[2].style.color = "blue"
+    // list[0].style.backgroundColor = "yellow";
+    // list[2].style.fontWeight = "bold";
+    // console.log(list);
+    // for (let i = 0; i < list.length; i++) { 
+    //   list[i].style.color = 'red'
+    // }
+
+
+    //GETELEMENTBYTAGNAME => selects all tags no matter what class or id
+    //  let list1 = document.getElementsByTagName('li');
+    //  list1[1].textContent = "changed";
+    //  list1[2].style.color = "black"
+    //  list1[0].style.backgroundColor = "yellow";
+    //  list1[2].style.fontWeight = "bold";
+
+
+    //QUERYSELECTOR
+    // //with semi colon you can select first-child, last-child and nth-child(2)
+    // let secondList = document.querySelector("li:nth-child(2)");
+    // console.log(secondList)
+    // secondList.textContent = "this is changed by query selector second list"
+
+    // // greater sign will get direct child
+    // let firstList = document.querySelector("ul > li");
+    // firstList.innerText = "changed by queryselector direct child element"
+
+    // //space will catch any child does not matter nested or direct
+    // let mysterious = document.querySelector("ul p");
+    // console.log(mysterious.textContent)
+
+    // let selectAll = document.querySelectorAll('li')
+    
+    // // selectAll.forEach((item)=> { 
+    // //   item.style.color = 'purple'
+    // // })
+
+    // console.log(selectAll);
+    // for (let i = 0; i < selectAll.length; i+=2) { 
+    //   console.log(selectAll[i])
+    //   selectAll[i].style.backgroundColor = "brown"
+      
+    // }
 
 });
